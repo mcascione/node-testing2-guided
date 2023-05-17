10c6218e2bfe17507b24aae6337557ebcdd1e3fb
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-17 11:00:39 -0700

    added packages and tests

--- api/server.js
+++ api/server.js
@@ -12,28 +12,28 @@ server.get("/", (req, res) => {
 
 server.get("/hobbits", (req, res) => {
   Hobbits.getAll()
-    .then(hobbits => {
+    .then((hobbits) => {
       res.status(200).json(hobbits);
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json(error);
     });
 });
 
-server.get("/hobbits/id", (req, res) => {
-  res.end()
+server.get("/hobbits/:id", (req, res) => {
+  res.end();
 });
 
-server.post("/hobbits", (req, res) => {
-  res.end()
+server.post("/hobbits", async (req, res) => {
+  res.status(201).json(await Hobbits.insert(req.body));
 });
 
 server.delete("/hobbits/:id", (req, res) => {
-  res.end()
+  res.end();
 });
 
 server.put("/hobbits/:id", (req, res) => {
-  res.end()
+  res.end();
 });
 
 module.exports = server;
